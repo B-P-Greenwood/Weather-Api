e215258d1ebcb8c9b0cebd29b074b16a549ef642
BPGreenwood <53783989+B-P-Greenwood@users.noreply.github.com>
2022-06-03 15:55:16 +0100

    -Started on the front end for fist MVP stage

--- models/weather.js
+++ models/weather.js
@@ -1,13 +1,74 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const apikey = process.env.KEY;  
+const apikey = process.env.KEY; 
+ 
+import fetch from "node-fetch";
+import queryString from 'query-string';
+import moment from "moment";
+
+// set the Timelines GET endpoint as the target URL
+const getTimelineURL = "https://api.tomorrow.io/v4/timelines";
+
+// pick the location, as a latlong pair
+let location = [52.4862, -1.8904];
+
+// list the fields
+const fields = [
+  "precipitationIntensity",
+  "precipitationType",
+  "windSpeed",
+  "windGust",
+  "windDirection",
+  "temperature",
+  "temperatureApparent",
+  "cloudCover",
+  "cloudBase",
+  "cloudCeiling",
+  "weatherCode",
+];
+
+// choose the unit system, either metric or imperial
+const units = "metric";
+
+// set the timesteps, like "current", "1h" and "1d"
+const timesteps = ["current", "1h", "1d"];
+
+// configure the time frame up to 6 hours back and 15 days out
+const now = moment.utc();
+const startTime = moment.utc(now).add(0, "minutes").toISOString();
+const endTime = moment.utc(now).add(1, "days").toISOString();
+
+// specify the timezone, using standard IANA timezone format
+const timezone = "Europe/London";
+
+// request the timelines with all the query string parameters as options
+const getTimelineParameters =  queryString.stringify({
+    apikey,
+    location,
+    fields,
+    units,
+    timesteps,
+    startTime,
+    endTime,
+    timezone,
+}, {arrayFormat: "comma"});
 
 export async function getWeather(){
+    const data = await fetch(getTimelineURL + "?" + getTimelineParameters, {method: "GET", compress: true})
+    .then((result) => result.json())
+    .catch((error) => console.error("error: " + err));
+    return data;
+};
+/*
 
-      const data = await fetch(`https://api.tomorrow.io/v4/timelines?location=-73.98529171943665,40.75872069597532&fields=temperature&timesteps=1h&units=metric&apikey=$`, {method: "GET", compress: true})
+/*
+export async function getWeather(){
+
+      const data = await fetch(`https://api.tomorrow.io/v4/timelines?location=-73.98529171943665,40.75872069597532&fields=temperature&timesteps=1h&units=metric&apikey=${apikey}`, {method: "GET", compress: true})
       .then((result) => result.json())
       .catch((error) => console.error("error: " + err));
 
       return data;
   };
+  */
--- plan.md
+++ plan.md
@@ -2,4 +2,8 @@
 -Set up CRUD routes for ClimaCell API
     -have routes folder and file 
     -have model folder and file 
-    -only need get and get specific 
+    -only need get
+-set up basic front end to show weather now for hard coded place
+-add more functions 
+    -ability to look forward in time for the weather 
+    -add user login page to save location for future usage 
