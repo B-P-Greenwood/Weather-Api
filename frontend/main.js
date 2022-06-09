56f46133ce658ee72a50c005a2eb4131ddd863ad
BPGreenwood <53783989+B-P-Greenwood@users.noreply.github.com>
2022-06-09 12:29:40 +0100

    Refactered the code in Main.js

--- frontend/main.js
+++ frontend/main.js
@@ -130,14 +130,7 @@ function createReport(report) {
   const windDirection = report.values.windDirection;
   const windGust = report.values.windGust;
   const windSpeed = report.values.windSpeed;
-
-  
-  const weatherImage = document.createElement("object");
-  weatherImage.data=setWeatherImage(weather);
-  weatherImage.width="150";
-  weatherImage.height="150";
-  hourly.appendChild(weatherImage);
-
+ 
   const values = [];
   values.push(`${cloudBase} km`);
   values.push(`${cloudCeiling} km`);
@@ -149,18 +142,27 @@ function createReport(report) {
   values.push(`${windGust} km/h`);
   values.push(`${windSpeed} km/h`);
 
-  //sets the values in the clouds section of webpage
-  for(let i=0; i<4; i++){
-    let p = document.createElement("p");
-    p.innerText = fields[i]+values[i];
-    clouds.appendChild(p); 
-  }
-  //sets the values in windTemp section of webpage
-  for(let i=4; i<values.length; i++){
-    let p = document.createElement("p");
-    p.innerText = fields[i+1]+values[i];
-    windTemp.appendChild(p); 
-  }
+  setWindTempSection(values);
+  setCloudSection(values);
+  setWeatherType(weather);
+  setRainType(rainType);
+  setTime();
+
+  const weatherImage = document.createElement("object");
+  weatherImage.data=setWeatherImage(weather);
+  weatherImage.width="150";
+  weatherImage.height="150";
+  hourly.appendChild(weatherImage);
+  
+};
+
+function setTime(){
+  let currnetTime = wReport.timelines[0].intervals[location].startTime.slice(11,16);
+  place.innerText = `Birmingham : ${currnetTime}`
+  tempField.innerText = `${Math.ceil(tempreture)} \u2103`;
+}
+
+function setRainType(rainType){
   //sets the rain type according to precipitationType
   for(let i=0; i<pKeys.length; i++){
     if(Number(pKeys[i])=== Number(rainType)){
@@ -169,18 +171,33 @@ function createReport(report) {
       clouds.appendChild(p); 
     }
   }
+}
 
-  let currnetTime = wReport.timelines[0].intervals[location].startTime.slice(11,16);
-  place.innerText = `Birmingham : ${currnetTime}`
-  tempField.innerText = `${Math.ceil(tempreture)} \u2103`;
-
+function setWeatherType(weather){
   //sets the weather type title according to weatherCodes object
   for(let i=0; i<wKeys.length; i++){
     if(Number(wKeys[i])=== Number(weather)){
       overview.innerText = weatherCodes[wKeys[i]];
     }
-  }
+  } 
+}
 
-};
+function setCloudSection(values){
+  //sets the values in the clouds section of webpage
+  for(let i=0; i<4; i++){
+    let p = document.createElement("p");
+    p.innerText = fields[i]+values[i];
+    clouds.appendChild(p); 
+  }
+}
+
+function setWindTempSection(values){
+  //sets the values in windTemp section of webpage
+  for(let i=4; i<values.length; i++){
+    let p = document.createElement("p");
+    p.innerText = fields[i+1]+values[i];
+    windTemp.appendChild(p); 
+  }
+}
 
 getWeather();
